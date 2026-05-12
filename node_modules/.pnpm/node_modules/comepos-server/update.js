6f527fefae3982a0ae4dc1b1a89b2edb5bb01229
enma <isaiasbbenitez457@gmail.com>
2026-05-12 20:23:10 -0300

    Preparado para despliegue: Configuración de CORS, variables de entorno y guía

--- node_modules/.pnpm/node_modules/comepos-server/update.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function main() {
-    await prisma.$executeRawUnsafe(`UPDATE "Client" SET category = 'FUNCIONARIO'`);
-    console.log("Clients updated");
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
