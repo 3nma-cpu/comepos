7041e42b1da509c872088a8a7f59ad4f20d9bcb0
enma <isaiasbbenitez457@gmail.com>
2026-05-14 19:05:39 -0300

    Fix: Disabled asset hashing to prevent Cloudflare 500 errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
+    },
     server: {
         port: 3000,
         open: true,
